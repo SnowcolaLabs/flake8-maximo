89a97d9e738396c22abf0b507e67821486cdfbf8
Anthony Keelan <anthony.keelan@gmail.com>
2023-04-12 12:36:28 -0600

    FM-5 feat: add tests for mbo count checking

--- tests/flake8_maximo_test.py
+++ tests/flake8_maximo_test.py
@@ -3,12 +3,30 @@ from typing import Set
 
 from flake8_maximo import Plugin
 
-
-def _resuts(s: str) -> Set[str]:
+def _results(s: str) -> Set[str]:
     tree = ast.parse(s)
     plugin = Plugin(tree)
-    return {f"{line}:{col} {msg}" for line, col, msg, _ in plugin.run()}
+    return {f"{line}:{col + 1} {msg}" for line, col, msg, _ in plugin.run()}
 
+def test_trivial():
+    assert _results("""""") == set()
 
-def test_trivia_case():
-    assert _resuts("") == set()
+def test_mbo_set_multiple_count_with_no_assignment():
+    test_code = """
+testSet = mbo.getMboSet('TESTSET')
+if testSet.count() == 0:
+    pass
+if testSet.count() >3:
+    pass
+    """
+    ret = _results(test_code)
+    assert ret == {'5:4 MAX100 count() called on mboSet: testSet more than once'}
+
+def test_mbo_set_count_in_loop():
+    test_code = """
+testSet = mbo.getMboSet('TESTSET')
+for x in range(5):
+    testSet.count()
+    """
+    ret = _results(test_code)
+    assert ret == {'4:5 MAX101 count() called on mboSet: testSet within a loop'}
